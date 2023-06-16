79fe2c18020c85282237bec8b815801231270899
david-leverenz <david.leverenz@gmail.com>
2023-06-16 15:30:06 -0500

    fixed reload bug, not a reload thing at all!

--- server.js
+++ server.js
@@ -25,21 +25,23 @@ const database = require('./db/db.json');
 app.use(express.static('public'));
 
 // Enables the route to the notes.html - button works - they have to put /notes in the URL (through a button normally) to take them to the public/notes.html file.
-app.get('/notes', (req, res) =>
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+});
 
 // I think this sets up so that I can get the database contents
 app.get('/api/notes', (req, res) => {
-  // console.log(database) // it's working
-  res.json(database);
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
 });
 
 // POST request to add a note
-app.post('/api/notes', (req, res, next) => {
+app.post('/api/notes', (req, res) => {
   // console.info(`${req.method} request received to add a note`);
   const { title, text } = req.body;
-  if (title && text ) {
+  if (title && text) {
     const newNote = {
       title,
       text,
@@ -57,51 +59,32 @@ app.post('/api/notes', (req, res, next) => {
           : console.log(
             `Task for ${newNote.title} has been written to JSON file`
           )
-          // return notes;
-      );
+        );
     })
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
+  
     console.log('I got to it');
-    res.redirect('/')
-    next();
-  } else {
+    res.json(notes)
+    } else {
     res.status(500).json('Error in posting review');
   }
 });
 
 app.delete('/api/notes/:id', (req, res) => {
   console.log("req params", req.params.id);
-
+// const id = req.params.id
   fs.readFile('./db/db.json', "utf8", (error, data) => {
-    // error ? console.log(error) : console.log(data);
+    if (error) throw error;
     const notes = JSON.parse(data)
-    // console.log(notes);
-    // console.log("=======================");
 
-    const result = notes.filter(notes => notes.id !== req.params.id)
-    // console.log(result);
+    const result = notes.filter(notes => notes.id !== req.params.id);
 
-    fs.writeFile(`./db/db.json`, JSON.stringify(result, null, 2), (err) =>
+    fs.writeFile('./db/db.json', JSON.stringify(result, null, 2), (err) =>
       err
         ? console.error(err)
-        : console.log(
-          `Task for ${req.params.id} has been taken from JSON file`
+        : res.json(result)
         )
-    );
-  })
-  // const response = {
-  //   status: 'success',
-  //   body: newNote,
-  // };
-//   res.status(201).json(response);
-// } else {
-//   res.status(500).json('Error in posting review');
+      })
 });
-
-
 
 // This puts a listener on this port, it will always be the last thing in your file.
 app.listen(PORT, () =>
